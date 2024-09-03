2cbaf73f3921b9ea433067a9d08f89297e65a0b5
virgile-am <ndayambaje.virgile@amalitechtraining.org>
2024-09-03 09:50:48 +0200

    initial commit

---
+++ src/main/java/com/concurrecnycollectionslab/concurrencylab/ConcurrencylabApplication.java
@@ -0,0 +1,13 @@
+package com.concurrecnycollectionslab.concurrencylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencylabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencylabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concurrecnycollectionslab/concurrencylab/ConcurrencylabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurrecnycollectionslab.concurrencylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencylabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
